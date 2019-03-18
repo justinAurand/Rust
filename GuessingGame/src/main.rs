aed9952548973071638241d36585a605e76289cb
Justin Aurand <justin.aurand@swiftcapital.com>
2019-03-18 10:47:26 -0400

    refine guessing game

--- GuessingGame/src/main.rs
+++ GuessingGame/src/main.rs
@@ -4,22 +4,31 @@ use rand::Rng;
 
 fn main() {
     println!("Guess the number from 1 to 100!");
-    println!("Please input your guess.");
-
-    let mut guess = String::new();
-    io::stdin().read_line(&mut guess)
-        .expect("Failed to read line");
-    let guess: u32 = guess.trim().parse()
-        .expect("Please type a number!");
-
-    println!("You guessed: {}", guess);
-
     let secret_number = rand::thread_rng().gen_range(1, 101);
-    match guess.cmp(&secret_number) {
-        Ordering::Less => println!("Too small!"),
-        Ordering::Greater => println!("Too big!"),
-        Ordering::Equal => println!("You win!"),
-    }
 
-    println!("The secret number was: {}", secret_number);
+    loop {
+        println!("Please input your guess.");
+
+        let mut guess = String::new();
+        io::stdin().read_line(&mut guess)
+            .expect("Failed to read line");
+        let guess: u32 = match guess.trim().parse() {
+            Ok(num) => num,
+            Err(_) => {
+                println!("You guess must be a number.");
+                continue;
+            }
+        };
+
+        println!("You guessed: {}", guess);
+
+        match guess.cmp(&secret_number) {
+            Ordering::Less => println!("Too small!"),
+            Ordering::Greater => println!("Too big!"),
+            Ordering::Equal => {
+                println!("You win!");
+                break;
+            }
+        }
+    }
 }
